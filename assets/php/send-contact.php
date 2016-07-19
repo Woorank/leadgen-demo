de13c55f22eda7224097b6c966e07369ec80014a
Nils De Moor <nils.de.moor@gmail.com>
2016-07-19 16:44:52 +0200

    Initial commit

---
+++ assets/php/send-contact.php
@@ -0,0 +1,14 @@
+<?php
+// Please put your PHP code here
+//
+// You can use entered name variable as $_POST['contact_name'], e-mail variable as $_POST['contact_email'], message variable as $_POST['contact_message']
+// Please insert here your email address: PUT_YOUR_EMAIL_ADDRESS_HERE
+$to      = 'PUT_YOUR_EMAIL_ADDRESS_HERE';
+
+$subject = 'Message from : '.$_POST['contact_name'];
+$message = "\r\n  subject : " .$_POST['contact_title']. "\r\n Message : " .$_POST['contact_message'];
+$headers = 'From: '.$_POST['contact_email']. "\r\n" .
+    'Reply-To: '.$_POST['contact_email']. "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, $message, $headers);
+?>
